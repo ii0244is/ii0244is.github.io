9d9bf550e815813a220549dbd39d602122401733
ii0244is <ii0244is@yahoo.co.jp>
2026-09-25 22:24:11 +0900

    update video player

--- VideoPlayer/script.js
+++ VideoPlayer/script.js
@@ -1,7 +1,29 @@
 const init = () => {
+  const timeRangeSlider = new TimeRangeSlider("time-range-slider");
   const videoPlayer = document.getElementById("video");
-
+  const startTimeInput = document.getElementById("startTime");
+  const endTimeInput = document.getElementById("endTime");
   const currentTimeDisplay = document.getElementById("currentTime");
+
+  const updateStartTime = (startTime) => {
+    startTimeInput.value = startTime.toFixed(3);
+    const endTime = parseFloat(endTimeInput.value);
+    const duration = videoPlayer.duration;
+    timeRangeSlider.setRange(startTime / duration, endTime / duration);
+    const currentTime = videoPlayer.currentTime;
+    if (currentTime < startTime) {
+      videoPlayer.currentTime = startTime;
+      timeRangeSlider.setCurrent(startTime / duration);
+    }
+  }
+
+  const updateEndTime = (endTime) => {
+    endTimeInput.value = endTime.toFixed(3);
+    const startTime = parseFloat(startTimeInput.value);
+    const duration = videoPlayer.duration;
+    timeRangeSlider.setRange(startTime / duration, endTime / duration);
+  }
+
   const updateCurrentTime = () => {
     const currentTime = Math.floor(videoPlayer.currentTime);
     const duration = Math.floor(videoPlayer.duration);
@@ -47,43 +69,40 @@ const init = () => {
     }
   });
 
-  const startTimeInput = document.getElementById("startTime");
-  startTimeInput.addEventListener("blur", () => {
+  startTimeInput.addEventListener("change", () => {
     let startTime = parseFloat(startTimeInput.value);
     const endTime = parseFloat(endTimeInput.value);
     if (startTime >= endTime) {
-      startTimeInput.value = endTime - 0.1;
-      startTime = parseFloat(startTimeInput.value);
+      startTime = endTime - 0.1;
     }
-    const currentTime = videoPlayer.currentTime;
-    if (currentTime < startTime) {
+    if (videoPlayer.currentTime < startTime) {
       videoPlayer.currentTime = startTime;
     }
+    updateStartTime(startTime);
   });
 
   const setStartTimeButton = document.getElementById("setStartTime");
   setStartTimeButton.addEventListener("click", () => {
-    const currentTime = videoPlayer.currentTime.toFixed(3);
-    startTimeInput.value = currentTime
+    const currentTime = videoPlayer.currentTime;
+    updateStartTime(currentTime);
   });
 
-  const endTimeInput = document.getElementById("endTime");
-  endTimeInput.addEventListener("blur", () => {
+  endTimeInput.addEventListener("change", () => {
     const startTime = parseFloat(startTimeInput.value);
     let endTime = parseFloat(endTimeInput.value);
     if (endTime <= startTime) {
-      endTimeInput.value = startTime + 0.1;
-      endTime = parseFloat(endTimeInput.value);
+      endTime = startTime + 0.1;
     }
     if (endTime <= videoPlayer.currentTime) {
       videoPlayer.currentTime = endTime;
     }
+    updateEndTime(endTime);
   });
 
   const setEndTimeButton = document.getElementById("setEndTime");
   setEndTimeButton.addEventListener("click", () => {
-    const currentTime = videoPlayer.currentTime.toFixed(3);
-    endTimeInput.value = currentTime
+    const currentTime = videoPlayer.currentTime;
+    updateEndTime(currentTime);
   });
 
   const speedInput = document.getElementById("speed");
@@ -100,6 +119,8 @@ const init = () => {
     endTimeInput.min = 0;
     endTimeInput.max = duration;
     endTimeInput.value = duration;
+    timeRangeSlider.setRange(0, 1);
+    timeRangeSlider.resize();
     updateCurrentTime();
   });
 
@@ -107,6 +128,8 @@ const init = () => {
     const startTime = parseFloat(startTimeInput.value);
     const endTime = parseFloat(endTimeInput.value);
     const currentTime = videoPlayer.currentTime;
+    const duration = videoPlayer.duration;
+    timeRangeSlider.setCurrent(currentTime / duration);
     if (currentTime < startTime) {
       videoPlayer.currentTime = startTime;
     } else if (endTime <= videoPlayer.currentTime) {
@@ -115,10 +138,122 @@ const init = () => {
     }
     updateCurrentTime();
   });
+
+  timeRangeSlider.onChangeStart = (value) => {
+    const duration = videoPlayer.duration;
+    const startTime = value * duration;
+    updateStartTime(startTime);
+  }
+
+  timeRangeSlider.onChangeEnd = (value) => {
+    const duration = videoPlayer.duration;
+    const endTime = value * duration;
+    updateEndTime(endTime);
+  }
+
+  timeRangeSlider.onChangeCurrent = (value) => {
+    const duration = videoPlayer.duration;
+    const currentTime = value * duration;
+    videoPlayer.currentTime = currentTime;
+    timeRangeSlider.setCurrent(currentTime / duration);
+  }
 }
 
 const secondsToTimeString = (seconds) => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = Math.floor(seconds % 60);
   return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
+}
+
+class TimeRangeSlider {
+  constructor(domId) {
+    this.dom = document.getElementById(domId);
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = this.dom.clientWidth;
+    this.canvas.height = this.dom.clientHeight;
+    this.dom.appendChild(this.canvas);
+    this.current = 0;
+    this.start = 0;
+    this.end = 1;
+    this.clicked = '';
+    this.canvas.addEventListener("pointerdown", (event) => this.pointerDown(event));
+    this.canvas.addEventListener("pointermove", (event) => this.pointerMove(event));
+    this.canvas.addEventListener("pointerup", () => this.clicked = '');
+    window.addEventListener("resize", () => this.resize());
+    this.resize();
+  }
+
+  setCurrent = (current) =>{
+    this.current = current;
+    this.draw();
+  }
+
+  setRange = (start, end) => {
+    this.start = start;
+    this.end = end;
+    this.draw();
+  }
+
+  resize = () => {
+    this.canvas.width = this.dom.clientWidth;
+    this.canvas.height = this.dom.clientHeight;
+    this.draw();
+  }
+
+  draw = () => {
+    const ctx = this.canvas.getContext("2d");
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    const width = this.canvas.width;
+    const height = this.canvas.height;
+    const startX = this.start * width;
+    const endX = this.end * width;
+    const currentX = this.current * width;
+
+    ctx.fillStyle = "#ccc";
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = "#666";
+    ctx.fillRect(0, 0, startX, height);
+    ctx.fillStyle = "#46c";
+    ctx.fillRect(startX, 0, currentX - startX, height);
+    ctx.fillStyle = "#666";
+    ctx.fillRect(endX, 0, width - endX, height);
+
+    const KNOB_WIDTH = 8;
+    ctx.fillStyle = "#f00";
+    ctx.fillRect(startX, 0, KNOB_WIDTH, height);
+    ctx.fillRect(endX - KNOB_WIDTH, 0, KNOB_WIDTH, height);
+  }
+
+  pointerDown = (event) => {
+    const x = event.offsetX;
+    const width = this.canvas.width;
+    const startX = this.start * width;
+    const endX = this.end * width;
+    const KNOB_WIDTH = 20;
+
+    if (startX - KNOB_WIDTH <= x && x <= startX + KNOB_WIDTH) {
+      this.clicked = 'start';
+    } else if (endX - KNOB_WIDTH <= x && x <= endX + KNOB_WIDTH) {
+      this.clicked = 'end';
+    } else if (startX <= x && x <= endX) {
+      this.clicked = 'current';
+    }
+  }
+
+  pointerMove = (event) => {
+    if (this.clicked === '') {
+      return;
+    }
+
+    const x = event.offsetX;
+    const width = this.canvas.width;
+    const value = x / width;
+    if (this.clicked === 'start') {
+      this.onChangeStart(Math.min(value, this.end));
+    } else if (this.clicked === 'end') {
+      this.onChangeEnd(Math.max(value, this.start));
+    } else if (this.clicked === 'current') {
+      this.onChangeCurrent(Math.min(Math.max(value, this.start), this.end));
+    }
+  }
 }
